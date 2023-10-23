cb2a4eb428f1c206c062a815d5640164d368fb1a
MichelOB <vildo.michel@gmail.com>
2023-10-22 21:12:41 -0300

    Alt em Calculadora

--- calculadora.c
+++ calculadora.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 double RelizaOperacao(double n1,double n2, char operacao)
 {
@@ -15,6 +16,7 @@ double RelizaOperacao(double n1,double n2, char operacao)
         } 
         return n1 / n2;
     default:
+        printf("Operacao invalida!");
         return 22;
     }
 }
@@ -22,21 +24,26 @@ double RelizaOperacao(double n1,double n2, char operacao)
 int main(){
     double num1 = 0, num2 = 0, resultado = 0;
     char op;
-    int quit = 0;
 
     printf("Modo de uso da calculadora: valor operacao valor.\nEx.: 1 + 1 (digitar somente numeros (= )");
+    printf("Para sair, pressione CTRL + C");
     do{
         printf("\n=> ");
+        //Recebe os numeros e a operação do usário
         scanf("%lf %c %lf", &num1, &op, &num2);
 
         printf("\t%.2lf %c %.2lf = ",num1, op, num2);
+        //reliza a operação e armazena em resultado
         resultado = RelizaOperacao(num1, num2, op);
         printf("%.2lf\n",resultado);
+
+        //zera todas as variaveis de inicio
         resultado = 0;
         num1 = 0;
         num2 = 0;    
 
-    } while(quit != 1);
+    } while(1);
+    
 
     return 0;
 }
